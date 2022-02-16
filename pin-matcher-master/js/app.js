87809d619a6d1381bd6eb4181580002ee59db4c8
rajib074 <rajib01711173241@gmail.com>
2022-02-16 13:44:26 +0600

    sedent commit

--- pin-matcher-master/js/app.js
+++ pin-matcher-master/js/app.js
@@ -27,3 +27,21 @@ document.getElementById('key-pad').addEventListener('click',function(event){
         calcInput.value = newNumber;
     }
 });
+function verifyPin(){
+    // console.log('going to verify')
+    const pin = document.getElementById('display-pin').value;
+    const typedNumber = document.getElementById('typed-number').value;
+    const notifySuccess = document.getElementById('notify-success');
+    const notifyFail = document.getElementById('notify-fail');
+    if (pin == typedNumber){
+        // const notifySuccess = document.getElementById('notify-success');
+        notifySuccess.style.display='block';
+        notifyFail.style.display='none';
+
+    }
+    else{
+        // const notifyFail = document.getElementById('notify-fail');
+        notifyFail.style.display='block';
+        notifySuccess.style.display='none';
+    }
+}
